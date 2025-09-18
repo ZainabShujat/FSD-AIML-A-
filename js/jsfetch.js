fb7749d4881b4dae9709400fc2a077d1f4fd3b1e
ZainabShujat <zainabshujatali@gmail.com>
2025-09-18 10:29:00 +0530

    added jsfetch.js

---
+++ js/jsfetch.js
@@ -0,0 +1,11 @@
+const username = "ZainabShujat"; // replace with your GitHub username
+
+fetch(`https://api.github.com/users/${ZainabShujat}`)
+  .then(response => response.json())
+  .then(data => {
+    console.log("GitHub Info:", data);
+    // You can access fields like data.name, data.public_repos, etc.
+  })
+  .catch(error => {
+    console.error("Error fetching GitHub info:", error);
+  });
