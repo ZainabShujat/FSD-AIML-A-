cd7c3c38e15ade0176d21e47f7e062dadbef9937
zainab shujat <Zainabshujatali@gmail.com>
2025-10-16 11:56:50 +0530

    Create src/app.jsx

---
+++ src/app.jsx
@@ -0,0 +1,15 @@
+export default function App() {
+  return (
+    <div className="card">
+      <img
+        src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTv_vuIWdS4z5Somcctla6_8buv6GTlsaZjOL8yQrvC_glExnt_M3Qrm0s&s"
+        width={250}
+        height={250}
+        alt="Physics Book Image"
+      />
+      <h4>Title: Physics</h4>
+      <h3>Price: ₹324/-</h3>
+      <button>Add To Cart</button>
+    </div>
+  )
+}
